7e58a91be030962c12c2876319dceb23d312a32e
Windmill-City <1449182174@qq.com>
2020-03-29 23:02:18 +0800

    Add STALauncher

---
+++ STALauncher/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace STALauncher
+{
+    public static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            SHFILEINFO shinfo = new SHFILEINFO();
+            Win32.SHGetFileInfo("StardewModdingAPI.exe", 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), Win32.SHGFI_ICON | Win32.SHGFI_LARGEICON);
+            Icon myIcon = Icon.FromHandle(shinfo.hIcon);
+            Win32.SetConsoleIcon(myIcon.Handle);
+            Type program = Type.GetType("StardewModdingAPI.Program, " + "StardewModdingAPI", false);
+            MethodInfo main = program.GetMethod("Main", BindingFlags.Public | BindingFlags.Static);
+            main.Invoke(null, new object[] { args });
+        }
+
+    }
+
+    //https://www.cnblogs.com/zhangtao/archive/2011/04/25/2027246.html
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SHFILEINFO
+    {
+        public IntPtr hIcon;
+        public IntPtr iIcon;
+        public uint dwAttributes;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string szDisplayName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+        public string szTypeName;
+    };
+    class Win32
+    {
+        public const uint SHGFI_ICON = 0x100;
+        public const uint SHGFI_LARGEICON = 0x0; // 'Large icon
+        public const uint SHGFI_SMALLICON = 0x1; // 'Small icon
+        [DllImport("shell32.dll")]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+        [DllImport("shell32.dll")]
+        public static extern uint ExtractIconEx(string lpszFile, int nIconIndex, int[] phiconLarge, int[] phiconSmall, uint nIcons);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetConsoleIcon(IntPtr hIcon);
+    }
+}
