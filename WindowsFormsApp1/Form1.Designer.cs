5ada9cb17a60cb39921eb7d1e49fd9d4b9cd2aeb
Windmill-City <1449182174@qq.com>
2020-04-21 11:50:12 +0800

    cleanup project files

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.listView1.Size = new System.Drawing.Size(413, 361);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // Form1
             // 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -5,25 +5,9 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        TSF tsf;
         public Form1()
         {
             InitializeComponent();
-
-            this.tsf = new TSF();
-            tsf.AssociateFocus(this.Handle);
-            tsf.CreateContext(this.Handle);
-            tsf.PushContext();
-            tsf.SetEnable(true);
-            tsf.SetTextExt(0, 0, 0, 0);
-
-            this.Move += Form1_Move;
-            listView1.Enabled = false;//Do not let the control get the focus
-            this.listView1.View = View.Details;
-            this.listView1.GridLines = true;
-            this.listView1.Columns.Add("Text", 100, HorizontalAlignment.Left);
-            this.listView1.Columns.Add("AcpStart", 100, HorizontalAlignment.Left);
-            this.listView1.Columns.Add("AcpEnd", 100, HorizontalAlignment.Left);
         }
         private const int EM_REPLACESEL = 0x00C2;
         private const int EM_SETSEL = 0x00B1;
@@ -34,23 +18,15 @@ namespace WindowsFormsApp1
             switch (m.Msg)
             {
                 case EM_REPLACESEL:
-                    var text = Marshal.PtrToStringAuto(m.LParam);
-                    item.Text = text;
-                    listView1.Items.Add(item);
-                    this.listView1.EnsureVisible(this.listView1.Items.Count - 1);
                     break;
                 case EM_SETSEL:
-                    item.SubItems.Add("" + m.WParam);
-                    item.SubItems.Add("" + m.LParam);
-                    listView1.Items.Add(item);
-                    this.listView1.EnsureVisible(this.listView1.Items.Count - 1);
                     break;
             }
         }
 
         private void Form1_Move(object sender, System.EventArgs e)
         {
-            tsf.SetTextExt(0, 0, 0, 0);
+            
         }
     }
 }
