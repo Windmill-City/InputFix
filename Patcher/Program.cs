bc9ce6ae421d57ee5277cce2ec29b01bb96f0937
Windmill_City <1449182174@qq.com>
2020-09-22 18:32:28 +0800

    Fix filemode

--- Patcher/Program.cs
+++ Patcher/Program.cs
@@ -84,7 +84,7 @@ namespace Patcher
 
         private static void WriteDll(byte[] dlldata, string path)
         {
-            var file = new FileStream(path, FileMode.OpenOrCreate | FileMode.Truncate, FileAccess.ReadWrite);
+            var file = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
             file.Write(dlldata, 0, dlldata.Length);
             file.Flush();
             file.Close();
